d457e0dff4beec173ed324f5ca1d002265bf11eb
illescend <118377027+illescend@users.noreply.github.com>
2023-10-09 17:11:58 +0200

    Finished objects and working on creating the resource classes.

---
+++ src/main/java/resources/UserResource.java
@@ -0,0 +1,69 @@
+package resources;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import model.User;
+import model.UserType;
+
+@Path("/user")
+public class UserResource {
+
+    @Context
+    private UriInfo info;
+
+    @Context
+    private HttpServletRequest req;
+
+    private final String email;
+
+    public UserResource(UriInfo info, HttpServletRequest req, String email) {
+        this.info = info;
+        this.req =  req;
+        this.email = email;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUserDetails() {
+        User user = null;
+        //User user = UserDao.instance.getUsers().get(Integer.parseInt(email));
+        //String password = uQueries.getPassword(email);
+        //user.setPassword(password);
+        if(user == null) {
+            throw new RuntimeException("User not found");
+        }else {
+            return user;
+        }
+    }
+
+    /**
+     * Admin perms needed for this function
+     * @return
+     */
+    @Path("/permissions")
+    @GET
+    public Response switchPermissions() {
+        User admin = (User) req.getAttribute("user");
+        //User user = uQueries.getSingleUser(email);
+        if(admin.getUser_type().equals(UserType.ADMIN)) {
+            //TODO Query to update perms
+            return Response.ok().build();
+        }else {
+            return Response.status(Response.Status.FORBIDDEN).build(); //Used to handle invalid actions (check js)
+        }
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteUser() {
+        //TODO Query to delete user.
+        return Response.ok().build();
+    }
+}
