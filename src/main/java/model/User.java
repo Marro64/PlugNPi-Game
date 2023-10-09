b589a20c63a775dd54be699d8a35f240ffdbbf58
illescend <118377027+illescend@users.noreply.github.com>
2023-10-09 15:05:10 +0200

    Created user objects.

---
+++ src/main/java/model/User.java
@@ -0,0 +1,69 @@
+package model;
+
+public class User {
+    private int uid;
+    private String username;
+    private String email;
+    private String password;
+    private UserType usertype;
+
+    public User(int uid, String username, String email, String password, UserType usertype) {
+        this.uid = uid;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.usertype = usertype;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setU_id(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUser_type() {
+        return usertype;
+    }
+
+    public void setUser_type(UserType usertype) {
+        this.usertype = usertype;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "u_id=" + uid +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", user_type=" + usertype +
+                '}';
+    }
+}
+
