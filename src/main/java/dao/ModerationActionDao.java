94d32965a3a849a7c7dc751765f141d259a60c6a
kalin <k.zhelezanov@student.utwente.nl>
2023-11-06 12:30:12 +0100

    ModerationActionResource done

--- src/main/java/dao/ModerationActionDao.java
+++ src/main/java/dao/ModerationActionDao.java
@@ -20,7 +20,7 @@ public enum ModerationActionDao {
         int logId = ((JsonObject) addLogQuerry.get(0)).get("log_id").getAsInt();
         return logId;
     }
-    public JsonArray getAllScores()
+    public JsonArray getAllLogs()
     {
         return ORM.executeQuery(
           "SELECT * FROM project.moderation_action ORDER BY time_of_action DESC"
@@ -30,14 +30,14 @@ public enum ModerationActionDao {
     {
         int adminID = admin.getUid();
         return ORM.executeQuery(
-                "SELECT *  FROM project.account a, project.moderation_action s WHERE a.u_id = ? AND s.admin_id = a.u_id GROUP BY a.u_id" ,adminID
+                "SELECT a.username m.action_type u.username m.time_of_action FROM project.account a, project.account u, project.moderation_action m WHERE a.u_id = ? AND m.admin_id = a.u_id " ,adminID
         );
     }
     public JsonArray getLogsFromUser(User user)
     {
         int userID = user.getUid();
         return ORM.executeQuery(
-                "SELECT *  FROM project.account a, project.moderation_action s WHERE a.u_id = ? AND s.player_id = a.u_id GROUP BY a.u_id" ,userID
+                "SELECT a.username m.action_type u.username m.time_of_action FROM project.account a, project.account u, project.moderation_action m WHERE u.u_id = ? AND u.u_id = m.player_id" ,userID
         );
     }
 
